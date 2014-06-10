d73b537bf206086c11d02d8ef9aa391a34da44e9
Anand Bagmar <abagmar@gmail.com>
2014-06-10 12:15:49 +0530

    Anand - added specflow, webdriver as nuget package dependencies

---
+++ google/google/GoogleSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace google
+{
+    [TestClass]
+    public class GoogleSearchTest
+    {
+        [TestMethod]
+        public void SearchForBlog()
+        {
+            Console.WriteLine("Search for blog test");
+        }
+    }
+}
